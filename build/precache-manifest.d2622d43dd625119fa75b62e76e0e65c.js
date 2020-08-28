5550d53920d80b3b010282a82f3048f39680c929
Julia Kurianova <judy@sfdev.com>
2020-08-27 23:14:24 -0400

    Create build

---
+++ build/precache-manifest.d2622d43dd625119fa75b62e76e0e65c.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5d38132f86365f07a92d6db70a9efd3",
+    "url": "/around-react/build/index.html"
+  },
+  {
+    "revision": "fa917b5c6d918c643e4f",
+    "url": "/around-react/build/static/css/main.5c0fd4ea.chunk.css"
+  },
+  {
+    "revision": "f1665753c8d1f94bd9cd",
+    "url": "/around-react/build/static/js/2.6ee419ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/around-react/build/static/js/2.6ee419ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa917b5c6d918c643e4f",
+    "url": "/around-react/build/static/js/main.19c3ad0d.chunk.js"
+  },
+  {
+    "revision": "fa8e8ae6e71b087492d0",
+    "url": "/around-react/build/static/js/runtime-main.b0f1621e.js"
+  },
+  {
+    "revision": "09f4068bb74fc78ffbce74f5596d6aa5",
+    "url": "/around-react/build/static/media/Inter-Black.09f4068b.woff2"
+  },
+  {
+    "revision": "e37354838bf8078ac0d296217613533f",
+    "url": "/around-react/build/static/media/Inter-Black.e3735483.woff"
+  },
+  {
+    "revision": "4bd040df09ea4765148b5743b8493e5f",
+    "url": "/around-react/build/static/media/Inter-ExtraLight.4bd040df.woff"
+  },
+  {
+    "revision": "4d9f96f8504cd034ef7e5f25434ca550",
+    "url": "/around-react/build/static/media/Inter-ExtraLight.4d9f96f8.woff2"
+  },
+  {
+    "revision": "7a8cc7241f766a142e15b2948804e547",
+    "url": "/around-react/build/static/media/Inter-Medium.7a8cc724.woff"
+  },
+  {
+    "revision": "f6cf0a0bc5fce3307e2c426eb14eb752",
+    "url": "/around-react/build/static/media/Inter-Medium.f6cf0a0b.woff2"
+  },
+  {
+    "revision": "4dd66a113d54a7f9a1ae913049610617",
+    "url": "/around-react/build/static/media/Inter-Regular.4dd66a11.woff2"
+  },
+  {
+    "revision": "7c539936c4c8c822b59a1bcc6c08a6ec",
+    "url": "/around-react/build/static/media/Inter-Regular.7c539936.woff"
+  },
+  {
+    "revision": "d476a8b86d0b656cfa463c1148e96069",
+    "url": "/around-react/build/static/media/pen-hover-avatar.d476a8b8.svg"
+  },
+  {
+    "revision": "967a0a9f419fc3bc6557762e245de9b6",
+    "url": "/around-react/build/static/media/pen-hover.967a0a9f.svg"
+  },
+  {
+    "revision": "6bfdb35467f51d3ba1f70f8d4a9c2b59",
+    "url": "/around-react/build/static/media/pen.6bfdb354.svg"
+  }
+]);
